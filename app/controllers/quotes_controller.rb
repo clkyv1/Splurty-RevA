7908f238f4bfd10bdced1a45a895ed8731c47f67
Brian Randall <brian.lewis.randall@outlook.com>
2019-08-12 18:55:32 +0000

    Lesson 13

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,11 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+  def new
+  end
+  
+  def create
+    Quote.create(quote_params)
+  end
+
 end
